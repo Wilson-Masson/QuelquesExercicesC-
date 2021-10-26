754a9ff8592db1100b83aeca3972d223ee940c3c
frederic <mwambafrederic8@gmail.com>
2021-10-26 20:50:22 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace consolrAppExercise
+{
+    class Program
+   
+    {
+         public static double calculatrice(double valeur1,char op,)
+         {
+             double result ;
+             switch(op)
+             {
+                 case '+':
+                 result=valeur1+valeur2;
+                 break;
+                 case '-':
+                 result=valeur1-valeur2;
+                 break;
+                 case '*':
+                 result=valeur1*valeur2;
+                 break;
+                 case '/':
+                 result=valeur1/valeur2;
+                 break
+
+             }
+         }
+
+    }
+        static void Main(string[] args)
+        { //Affichage
+            // Console.WriteLine("Tapper votre nom:");
+            // string nom= Console.ReadLine();
+            // Console.WriteLine($"votre nom est:{nom}");
+            //  // les types de donnes
+            //  int a=34;
+            //  long b=34L;
+            //  char m='M';
+            //  string nome ="Francois";
+            //  bool bo=true;
+            //  float f=5,3;
+            //  double =54.3;
+            //  string="20";
+            //   Console.WriteLine("Tapper votre age:");
+              
+            //  int age=Convert.ToInt32(Console.ReadLine());
+            //  //les conditions:if et switch
+             
+            //  if (age >=30)
+            //  {
+            //      Console.WriteLine("vous etes adultes");
+            //  }
+            //  else if(age >=20)
+            //  {
+            //      Console.WriteLine("Vous etes adolescents");
+            //  }
+            //  else
+            //  Console.WriteLine("Vous etes mineur");
+            
+             
+        }
+    }
+}
