a507c06dbf92390d1281e183be6e869eb6421556
frederic <mwambafrederic8@gmail.com>
2021-10-27 03:16:55 +0200

    initial

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace consolrAppExercise
     class Program
    
     {
-         public static double calculatrice(double valeur1,char op,)
+         public static double calculatrice(double valeur1,char op,double valeur2)
          {
              double result ;
              switch(op)
@@ -21,14 +21,80 @@ namespace consolrAppExercise
                  break;
                  case '/':
                  result=valeur1/valeur2;
-                 break
+                 break;
+                 default:
+                 result=0;
+                 break;
 
              }
-         }
+             return result;
+         
 
     }
         static void Main(string[] args)
-        { //Affichage
+        { 
+            double result=calculatrice(34 ,'-',12);
+            Console.WriteLine("Entrez l'age ");
+            int age=Convert.ToInt32(Console.ReadLine());
+            //Expression ternaire
+            string texte =(age >18) ? "Mageur" : "Mineur";
+            Console.WriteLine(texte);
+
+            //string
+            string name ="Wilson Masson";
+            string[] names=name.Split('-');
+            foreach(var i in names)
+            {
+             Console.WriteLine(i);
+            }
+            
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(name.IndexOf('n'));
+            Console.WriteLine(name.Length);
+            Console.WriteLine(name.Substring(name.IndexOf('M')));
+            Console.WriteLine(name[0]);
+
+            //les boucles :while, do while , for, foreach
+            // int i=12;
+            // while (i<10)
+            // {
+            //     Console.WriteLine(i);
+            //              i++;
+            // }
+            // do while
+            //    {
+
+            //    }
+            // for(int i =10 ; i >= 0 ; i--)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            for(int i = 0 ; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            //foreach 
+            // foreach(char n in name){
+            //     Console.WriteLine(n);
+            // }
+            //arrays(tableaux)
+            // string [] names ={ "Wilson","Jean","Etienne","Ben","Elisa"};
+            int [] ages ={ 20,30,24,22};
+            //Autre Maniere de declaration de tableau
+            // string[] names =new string[]{ "Wilson","Jean","Etienne","Ben","Elisa"};
+            //  int [] ages=new int[]{ 20,30,24,22};
+            //pour afficher
+            foreach(string nom in names)
+            {
+                Console.WriteLine(nom);
+            }
+            foreach(int chiffre in ages)
+            {
+                Console.WriteLine(chiffre);
+            }
+            
+            //Affichage
             // Console.WriteLine("Tapper votre nom:");
             // string nom= Console.ReadLine();
             // Console.WriteLine($"votre nom est:{nom}");
@@ -56,8 +122,25 @@ namespace consolrAppExercise
             //  }
             //  else
             //  Console.WriteLine("Vous etes mineur");
+
+             string []namess={"Ella","Bosco"};
+             Console.WriteLine(namess.GetType());
+             Console.WriteLine("La taille est de:" + namess.Length);
+
+             namess=new string[]{"Ella","Bosco"};
+             foreach(var namet in namess)
+             {
+                 Console.WriteLine(namet);
+             }
+             Array.Sort(namess);
+             Array.Sort(namess);
             
-             
+
+             Console.WriteLine("Ordonner un tableau");
+             foreach(var namet in namess)
+             {
+                  Console.WriteLine(namet);
+             }
         }
     }
 }
